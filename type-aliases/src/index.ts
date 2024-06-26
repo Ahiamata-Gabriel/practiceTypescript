bddf89353e706673402db07e55fbabf5fc76ee5a
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-06-26 12:07:02 +0000

    type-aliases

---
+++ type-aliases/src/index.ts
@@ -0,0 +1,15 @@
+// using the PascalCase
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "Mosh",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
