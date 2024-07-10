33a8364b703a3b09f1240e3b94530a01c9015891
Gabriel Ahiamata <ahiamatamawuli@gmail.com>
2024-07-10 02:19:05 +0000

    16.index-signatures/

---
+++ 16.index-signatures/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+class SeatAssignment {}
+let seats = new SeatAssignment();
+seats.A1 = "Mosh";
+seats.A2 = "John";
+//# sourceMappingURL=index.js.map
+
+console.log(seats);
+console.log(SeatAssignment);
---
+++ 16.index-signatures/src/index.ts
@@ -0,0 +1,8 @@
+class SeatAssignment {
+  [setNumber: string]: string;
+}
+
+let seats = new SeatAssignment();
+
+seats.A1 = "Mosh";
+seats.A2 = "John";
